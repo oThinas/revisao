41cd2fb168e375e0be58b92f8b2ec838224d0228
oThinas <thiagomartinsprado@gmail.com>
2023-02-09 08:40:34 -0300

    fix: lógica da classe number ajustada

--- src/Number.java
+++ src/Number.java
@@ -5,17 +5,17 @@ public class Number {
   }
 
   private void checkEvenOrOdd(int number) {
+  if (number % 2 == 0) {
+    System.out.println(String.format("%d é par", number));
+  } else {
+    System.out.println(String.format("%d é ímpar", number));
+  }
+}
+
+private void checkPositiveOrNegative(int number) {
     if (number == 0) {
       System.out.println(String.format("%d é neutro", number));
-    } else if (number % 2 == 0) {
-      System.out.println(String.format("%d é par", number));
-    } else {
-      System.out.println(String.format("%d é ímpar", number));
-    }
-  }
-
-  private void checkPositiveOrNegative(int number) {
-    if (number > 0) {
+    } else if (number > 0) {
       System.out.println(String.format("%d é positvo", number));
     } else {
       System.out.println(String.format("%d é negativo", number));
